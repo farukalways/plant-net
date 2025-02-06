4fbc518ec3781aea76cbb2030bf0a7c077cd79a1
farukalways <alishopon367@gmail.com>
2025-02-06 12:49:46 +0600

    get all plant data and use tanstack query || plants.jsx

--- client/src/pages/Dashboard/Seller/AddPlant.jsx
+++ client/src/pages/Dashboard/Seller/AddPlant.jsx
@@ -1,7 +1,54 @@
-import { Helmet } from 'react-helmet-async'
-import AddPlantForm from '../../../components/Form/AddPlantForm'
+import { Helmet } from "react-helmet-async";
+import AddPlantForm from "../../../components/Form/AddPlantForm";
+import { imageUpload } from "../../../api/utils";
+import useAuth from "../../../hooks/useAuth";
+import { useState } from "react";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import toast from "react-hot-toast";
 
 const AddPlant = () => {
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+  const [uploadButtonText, setUploadButtonText] = useState("Upload Image");
+  const [loading, setLoading] = useState(false);
+
+  //handle form submite
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    const form = e.target;
+    const name = form.name.value;
+    const description = form.description.value;
+    const category = form.category.value;
+    const price = parseFloat(form.price.value);
+    const quantity = parseFloat(form.quantity.value);
+    const image = form.image.files[0];
+    //send image data to imgbb
+    const photoURL = await imageUpload(image);
+    //seiler info
+    const seiler = {
+      name: user?.displayName,
+      image: user?.photoURL,
+      email: user?.email,
+    };
+    const plantData = {
+      name,
+      description,
+      category,
+      price,
+      quantity,
+      image: photoURL,
+      seiler,
+    };
+    try {
+      await axiosSecure.post("/plants", plantData);
+      toast.success("Data Added Successfully");
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <div>
       <Helmet>
@@ -9,9 +56,14 @@ const AddPlant = () => {
       </Helmet>
 
       {/* Form */}
-      <AddPlantForm />
+      <AddPlantForm
+        handleSubmit={handleSubmit}
+        uploadButtonText={uploadButtonText}
+        setUploadButtonText={setUploadButtonText}
+        loading={loading}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default AddPlant
+export default AddPlant;
